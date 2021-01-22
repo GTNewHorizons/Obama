b79a00b72875b5baa38043c8eb0b3fbbb85a32e0
Tec <daniel112092@gmail.com>
2021-01-22 21:13:55 +0100

    Drop that bass

--- src/main/java/com/gtnewhorizons/gtppnt/main/tileentites/multi/definition/DefaultStructureDefinition.java
+++ src/main/java/com/gtnewhorizons/gtppnt/main/tileentites/multi/definition/DefaultStructureDefinition.java
@@ -2,6 +2,7 @@ package com.gtnewhorizons.gtppnt.main.tileentites.multi.definition;
 
 import com.github.bartimaeusnek.bartworks.util.Pair;
 import com.github.technus.tectech.mechanics.structure.IStructureDefinition;
+import com.github.technus.tectech.mechanics.structure.StructureDefinition;
 import com.github.technus.tectech.thing.metaTileEntity.multi.base.GT_MetaTileEntity_MultiblockBase_EM;
 import com.gtnewhorizons.gtppnt.main.utils.IAddsBlocks;
 import gregtech.api.util.GT_LanguageManager;
@@ -18,6 +19,8 @@ import java.util.stream.Stream;
 
 import static com.github.bartimaeusnek.bartworks.system.material.BW_GT_MaterialReference.*;
 import static com.github.bartimaeusnek.bartworks.system.material.WerkstoffLoader.*;
+import static com.github.technus.tectech.mechanics.structure.StructureUtility.*;
+import static com.github.technus.tectech.mechanics.structure.StructureUtility.ofHatchAdderOptional;
 import static com.gtnewhorizons.gtppnt.main.CommonValues.TM_MARK;
 import static com.gtnewhorizons.gtppnt.main.compat.bartworks.MaterialsClass.*;
 import static com.gtnewhorizons.gtppnt.main.loaders.CasingTextureLoader.getAdvancedCasingTextureIndex;
@@ -49,6 +52,145 @@ public enum DefaultStructureDefinition {
     LARGE_LATHE(CUBE_3x3x3_WithMuffler, BWBlockCasingsAdvanced, Staballoy.getmID(), null, 0, getAdvancedCasingTextureIndex(Staballoy.getmID())),
     LARGE_AUTOCLAVE(CUBE_3x3x3_WithMuffler, BWBlockCasingsAdvanced, Staballoy.getmID(), null, 0, getAdvancedCasingTextureIndex(Staballoy.getmID())),
     ;
+
+    public static final char REQUIRED_ANY_HATCH = 'c';
+    public static final char INPUT = 'i';
+    public static final char OUTPUT = 'o';
+    public static final char ENERGY = 'e';
+    public static final char DYNAMO = 'd';
+    public static final char MUFFLER = 'p';
+    public static final char MAINTENANCE = 'm';
+    public static final char BLOCK = 'b';
+    public static final char SPECIAL_BLOCK = 's';
+
+    public static final char OPTIONAL_ANY_HATCH = 'C';
+    public static final char OPTIONAL_INPUT = 'I';
+    public static final char OPTIONAL_OUTPUT = 'O';
+    public static final char OPTIONAL_ENERGY = 'E';
+    public static final char OPTIONAL_DYNAMO = 'D';
+    public static final char OPTIONAL_MUFFLER = 'P';
+    public static final char OPTIONAL_MAINTENANCE = 'M';
+    public static final char OPTIONAL_BLOCK = 'B';
+    public static final char OPTIONAL_SPECIAL_BLOCK = 'S';
+
+    /**
+     * Creates a Multiblock Definition
+     * <p></p>
+     * <p>Hints are: </p>
+     * <p> 1 Any hatch?/Bus</p>
+     * <p> 2 Input Hatch/Bus</p>
+     * <p> 3 Output Hatch/Bus</p>
+     * <p> 4 Energy Hatch</p>
+     * <p> 5 Dynamo Hatch</p>
+     * <p> 6 Muffler Hatch</p>
+     * <p> 7 Maintenance Hatch</p>
+     * <p> 8 Special Block</p>
+     *
+     * for structures you should make a new String[][]
+     *
+     *                     <p>each internal String[] defines a layer</p>
+     *                     <p>each string the blocks of the layer</p>
+     *                     <p>each char is a block definition</p>
+     *                     <p></p>
+     *                     <p>Allowed Characters, Uppercase means OPTIONAL:</p>
+     *                     <p>c = any Input/Output Hatch/Bus or Energy Input Hatch</p>
+     *                     <p>i = Input Hatch/Bus</p>
+     *                     <p>o = Output Hatch/Bus</p>
+     *                     <p>e = Energy Input Hatch</p>
+     *                     <p>d = Dynamo Hatch</p>
+     *                     <p>p = Muffler Hatch</p>
+     *                     <p>m = Maintenance Hatch</p>
+     *                     <p>s = Special Block (i.e. Coils)</p>
+     *                     <p>b = Block</p>
+     *                     <p></p>
+     *                     <p>- = air</p>
+     *                     <p>+ = non-air</p>
+     *                     <p>~ = controller</p>
+     *                     <p>(and other defined in expander)</p>
+     *                     <p></p>
+     * @param textureIndex the texture for the hatches
+     * @param setCasing    Casing Block which is used to build the Multiblock
+     * @param setMeta      Casing Block Meta which is used to build the Multiblock
+     * @return IStructureDefinition for your Multiblock
+     */
+    @SuppressWarnings("deprecation")
+    public static <T extends GT_MetaTileEntity_MultiblockBase_EM & IAddsBlocks> StructureDefinition.Builder<T> getDefaultStructureDefinitionBuilder(int textureIndex, Block setCasing, int setMeta) {
+        return StructureDefinition.<T>builder()
+                .addElement(BLOCK, ofBlock(setCasing, setMeta))
+                .addElement(OPTIONAL_BLOCK, ofBlock(setCasing, setMeta, Blocks.air, 0))
+                .addElement(SPECIAL_BLOCK, ofBlockAdder(IAddsBlocks::addBlockToMachine, 8))
+                .addElement(OPTIONAL_SPECIAL_BLOCK, ofBlockAdder(IAddsBlocks::addBlockToMachine, Blocks.air, 0))
+                .addElement(REQUIRED_ANY_HATCH, ofHatchAdder(
+                        GT_MetaTileEntity_MultiblockBase_EM::addClassicToMachineList,
+                        textureIndex,
+                        1))
+                .addElement(INPUT, ofHatchAdder(
+                        GT_MetaTileEntity_MultiblockBase_EM::addClassicInputToMachineList,
+                        textureIndex,
+                        2))
+                .addElement(OUTPUT, ofHatchAdder(
+                        GT_MetaTileEntity_MultiblockBase_EM::addClassicOutputToMachineList,
+                        textureIndex,
+                        3))
+                .addElement(ENERGY, ofHatchAdder(
+                        GT_MetaTileEntity_MultiblockBase_EM::addEnergyInputToMachineList,
+                        textureIndex,
+                        4))
+                .addElement(DYNAMO, ofHatchAdder(
+                        GT_MetaTileEntity_MultiblockBase_EM::addDynamoToMachineList,
+                        textureIndex,
+                        5))
+                .addElement(MUFFLER, ofHatchAdder(
+                        GT_MetaTileEntity_MultiblockBase_EM::addClassicMufflerToMachineList,
+                        textureIndex,
+                        6))
+                .addElement(MAINTENANCE, ofHatchAdder(
+                        GT_MetaTileEntity_MultiblockBase_EM::addClassicMaintenanceToMachineList,
+                        textureIndex,
+                        7))
+                .addElement(OPTIONAL_ANY_HATCH, ofHatchAdderOptional(
+                        GT_MetaTileEntity_MultiblockBase_EM::addClassicToMachineList,
+                        textureIndex,
+                        1,
+                        setCasing,
+                        setMeta))
+                .addElement(OPTIONAL_INPUT, ofHatchAdderOptional(
+                        GT_MetaTileEntity_MultiblockBase_EM::addClassicInputToMachineList,
+                        textureIndex,
+                        2,
+                        setCasing,
+                        setMeta))
+                .addElement(OPTIONAL_OUTPUT, ofHatchAdderOptional(
+                        GT_MetaTileEntity_MultiblockBase_EM::addClassicOutputToMachineList,
+                        textureIndex,
+                        3,
+                        setCasing,
+                        setMeta))
+                .addElement(OPTIONAL_ENERGY, ofHatchAdderOptional(
+                        GT_MetaTileEntity_MultiblockBase_EM::addEnergyInputToMachineList,
+                        textureIndex,
+                        4,
+                        setCasing,
+                        setMeta))
+                .addElement(OPTIONAL_DYNAMO, ofHatchAdderOptional(
+                        GT_MetaTileEntity_MultiblockBase_EM::addDynamoToMachineList,
+                        textureIndex,
+                        5,
+                        setCasing,
+                        setMeta))
+                .addElement(OPTIONAL_MUFFLER, ofHatchAdderOptional(
+                        GT_MetaTileEntity_MultiblockBase_EM::addClassicMufflerToMachineList,
+                        textureIndex,
+                        6,
+                        setCasing,
+                        setMeta))
+                .addElement(OPTIONAL_MAINTENANCE, ofHatchAdderOptional(
+                        GT_MetaTileEntity_MultiblockBase_EM::addClassicMaintenanceToMachineList,
+                        textureIndex,
+                        7,
+                        setCasing,
+                        setMeta));
+    }
 
     private final GeometricInstance geometrics;
     private final Block toBuildWith;
