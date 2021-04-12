ccb39e34442cb68bd21bcdd3a654150dd1c7dfd9
korneel vandamme <Krampus.sack.never@gmail.com>
2021-04-13 01:26:18 +0200

    add large compressor

---
+++ src/main/java/com/gtnewhorizons/gtppnt/main/tileentites/multi/definition/multies/GT_MetaTileEntity_TM_Large_Compressor.java
@@ -0,0 +1,131 @@
+package com.gtnewhorizons.gtppnt.main.tileentites.multi.definition.multies;
+
+import com.github.technus.tectech.mechanics.structure.IStructureDefinition;
+import com.github.technus.tectech.mechanics.structure.StructureDefinition;
+import com.github.technus.tectech.thing.metaTileEntity.multi.base.GT_MetaTileEntity_MultiblockBase_EM;
+import com.github.technus.tectech.util.Vec3Impl;
+import com.gtnewhorizons.gtppnt.main.tileentites.multi.definition.GT_MetaTileEntity_TM_Factory_Base;
+import com.gtnewhorizons.gtppnt.main.tileentites.multi.definition.GT_MetaTileEntity_TM_Factory_Sliceable;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import gregtech.api.util.GT_Recipe;
+import ic2.core.IC2;
+import ic2.core.Ic2Items;
+import ic2.core.block.BlockBase;
+import ic2.core.block.BlockTexGlass;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+
+import static com.github.technus.tectech.mechanics.structure.StructureUtility.*;
+import static com.github.technus.tectech.thing.casing.TT_Container_Casings.sHintCasingsTT;
+import static com.gtnewhorizons.gtppnt.main.compat.bartworks.MaterialsClass.MaragingSteel250;
+import static com.gtnewhorizons.gtppnt.main.compat.bartworks.MaterialsClass.Staballoy;
+
+public class GT_MetaTileEntity_TM_Large_Compressor extends GT_MetaTileEntity_TM_Factory_Sliceable {
+
+    public  GT_MetaTileEntity_TM_Large_Compressor(int aID) {
+       super(aID, "multimachine.tm.large_compressor", "Large Compressor");
+    }
+
+    public GT_MetaTileEntity_TM_Large_Compressor(String aName) {
+        super(aName);
+    }
+
+    @Override
+    public IMetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
+        return new GT_MetaTileEntity_TM_Large_Compressor(mName);
+    }
+
+    @Override
+    public String[] getDescription() {
+        return new String[]{"large comp desc"};
+    }
+
+    @Override
+    public IStructureDefinition<? extends GT_MetaTileEntity_MultiblockBase_EM> getMachineStructure() {
+        Block reinforcedGlass = Block.getBlockFromName("IC2:blockAlloyGlass");
+        return StructureDefinition.<GT_MetaTileEntity_TM_Factory_Base>builder()
+                .addShape(TM_STRUCTURE_START, new String[][]{
+                        {" AAAAA ","AGGGGGA","AGGGGGA","AGGGGGA","BBBB~BB"},
+                        {" AAAAA ","A-A-A-A","A-A-A-A","A-A-A-A","BAAAAAB"}
+                })
+                .addShape(TM_STRUCTURE_MIDDLE, new String[][]{
+                        {" AAAAA ","A-A-A-A","APA-APA","A-A-A-A"," AAAAA "},
+                        {" AAAAA ","A-A-A-A","A-A-A-A","A-A-A-A"," AAAAA "}
+                })
+                .addShape(TM_STRUCTURE_END,new String[][]{
+                        {"","","","","BAAAAAB"},
+                        {" AAAAA ","AGGGGGA","AGGGGGA","AGGGGGA","BBBBBBB"}
+                })
+                .addElement('A',ofBlock(getCasingBlock(),getCasingMeta()))
+                .addElement('B', ofChain(
+                        ofHatchAdder(GT_MetaTileEntity_TM_Factory_Base::addClassicToMachineList,
+                                getTextureIndex(), sHintCasingsTT,0),
+                                ofBlock(getCasingBlock(),getCasingMeta())
+                ))
+                .addElement('G',ofBlockAnyMeta(reinforcedGlass))
+                .addElement('P',ofHatchAdder(GT_MetaTileEntity_TM_Factory_Base::addPistonCasingToMachineList,
+                        getTextureIndex(),1))
+                .build();
+    }
+
+    @Override
+    public Vec3Impl getStartStructureOffset() {
+        return new Vec3Impl(4,4,0);
+    }
+
+    @Override
+    public Vec3Impl getSliceStructureOffset() {
+        return new Vec3Impl(4,4,-2);
+    }
+
+    @Override
+    public Vec3Impl getPerSliceOffset() {
+        return new Vec3Impl(0,0,-2);
+    }
+
+    @Override
+    public Vec3Impl getEndStructureOffset() {
+        return new Vec3Impl(0,0,1);
+    }
+
+    @Override
+    public int getMaxSlices() {
+        return 8;
+    }
+
+    @Override
+    public int getParalellsPerSlice() {
+        return 10;
+    }
+
+    @Override
+    public short getCasingMeta() {
+        return MaragingSteel250.getmID();
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public String[] getStructureDescription(ItemStack itemStack) {
+        return new String[]{"struct desc"};
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public String getMachineTextureName() {
+        return "TM_LARGE_COMPRESSOR";
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public String getMachineSoundName() {
+        return "fx_lo_freq";
+    }
+
+    @Override
+    public GT_Recipe.GT_Recipe_Map getRecipeMap() {
+        return GT_Recipe.GT_Recipe_Map.sCompressorRecipes;
+    }
+}
