053e7a58b081efe7609a33d59053f52ef55380b7
korneel vandamme <Krampus.sack.never@gmail.com>
2021-05-08 17:51:27 +0200

    use less code to add function casings

--- src/main/java/com/gtnewhorizons/gtppnt/main/loaders/HatchCasingLoader.java
+++ src/main/java/com/gtnewhorizons/gtppnt/main/loaders/HatchCasingLoader.java
@@ -1,6 +1,7 @@
 package com.gtnewhorizons.gtppnt.main.loaders;
 
 import com.gtnewhorizons.gtppnt.main.GTAFMod;
+import com.gtnewhorizons.gtppnt.main.items.CustomItemList;
 import com.gtnewhorizons.gtppnt.main.tileentites.single.hatches.GT_MetaTileEntity_TM_HatchCasing;
 import com.gtnewhorizons.gtppnt.main.tileentites.single.hatches.GT_MetaTileEntity_TM_Hatch_Catalytic_Muffler;
 import com.gtnewhorizons.gtppnt.main.tileentites.single.hatches.defenition.CasingFunction;
@@ -20,37 +21,38 @@ public class HatchCasingLoader {
     }
 
     //Uses ID Range of 31071 to 31100
+    //TODO change IDs to use the ones from alk
     private static void loadCasings() {
         int aID = 31071;
 
-        GRINDING_CASING_LV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.GRINDING, 1).getItem());
-        GRINDING_CASING_MV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.GRINDING, 2).getItem());
-        GRINDING_CASING_HV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.GRINDING, 3).getItem());
+        aID = setMultipleCasingTiered(CasingFunction.GRINDING,aID,
+                GRINDING_CASING_LV,GRINDING_CASING_MV,GRINDING_CASING_HV);
+        aID = setMultipleCasingTiered(CasingFunction.PISTON,aID,
+                PISTON_CASING_LV,PISTON_CASING_MV,PISTON_CASING_HV);
 
-        PISTON_CASING_LV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.PISTON, 1).getItem());
-        PISTON_CASING_MV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.PISTON, 2).getItem());
-        PISTON_CASING_HV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.PISTON, 3).getItem());
+        aID = setMultipleCasingTiered(CasingFunction.MOTOR,aID,
+                MOTOR_CASING_LV,MOTOR_CASING_MV,MOTOR_CASING_HV);
 
-        MOTOR_CASING_LV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.MOTOR, 1).getItem());
-        MOTOR_CASING_MV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.MOTOR, 2).getItem());
-        MOTOR_CASING_HV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.MOTOR, 3).getItem());
+        aID = setMultipleCasingTiered(CasingFunction.CIRCUIT,aID,
+                CIRCUIT_CASING_LV,CIRCUIT_CASING_MV,CIRCUIT_CASING_HV);
 
-        CIRCUIT_CASING_LV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.CIRCUIT, 1).getItem());
-        CIRCUIT_CASING_MV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.CIRCUIT, 2).getItem());
-        CIRCUIT_CASING_HV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.CIRCUIT, 3).getItem());
+        aID = setMultipleCasingTiered(CasingFunction.CONVEYOR,aID,
+                CONVEYOR_CASING_LV,CONVEYOR_CASING_MV,CONVEYOR_CASING_HV);
 
-        CONVEYOR_CASING_LV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.CONVEYOR, 1).getItem());
-        CONVEYOR_CASING_MV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.CONVEYOR, 2).getItem());
-        CONVEYOR_CASING_HV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.CONVEYOR, 3).getItem());
-
-        PUMP_CASING_LV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.PUMP, 1).getItem());
-        PUMP_CASING_MV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.PUMP, 1).getItem());
-        PUMP_CASING_HV.set(new GT_MetaTileEntity_TM_HatchCasing(aID++, CasingFunction.PUMP, 1).getItem());
+        aID = setMultipleCasingTiered(CasingFunction.PUMP,aID,
+                PUMP_CASING_LV,PISTON_CASING_MV,PUMP_CASING_HV);
 
         CATALYTIC_MUFFLER_EV.set(new GT_MetaTileEntity_TM_Hatch_Catalytic_Muffler(aID++, 4).getItem());
         CATALYTIC_MUFFLER_IV.set(new GT_MetaTileEntity_TM_Hatch_Catalytic_Muffler(aID++, 5).getItem());
         CATALYTIC_MUFFLER_LuV.set(new GT_MetaTileEntity_TM_Hatch_Catalytic_Muffler(aID++, 6).getItem());
         CATALYTIC_MUFFLER_ZPM.set(new GT_MetaTileEntity_TM_Hatch_Catalytic_Muffler(aID++, 7).getItem());
         CATALYTIC_MUFFLER_UV.set(new GT_MetaTileEntity_TM_Hatch_Catalytic_Muffler(aID++, 8).getItem());
+    }
+
+    private static int setMultipleCasingTiered(CasingFunction function,int aID,CustomItemList... itemList) {
+        for (int i = 0;i<itemList.length;i++) {
+            itemList[i].set(new GT_MetaTileEntity_TM_HatchCasing(aID++,function,(i+1)).getItem());
+        }
+        return aID;
     }
 }
