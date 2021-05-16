2d474f1962385f08a708af968c2df49151902dd4
korneel vandamme <Krampus.sack.never@gmail.com>
2021-05-16 04:34:50 +0200

    add large centrifuge

--- src/main/java/com/gtnewhorizons/gtppnt/main/tileentites/multi/instances/GT_MetaTileEntity_TM_Large_Centrifuge.java
+++ src/main/java/com/gtnewhorizons/gtppnt/main/tileentites/multi/instances/GT_MetaTileEntity_TM_Large_Centrifuge.java
@@ -2,22 +2,26 @@ package com.gtnewhorizons.gtppnt.main.tileentites.multi.instances;
 
 import com.github.technus.tectech.mechanics.structure.IStructureDefinition;
 import com.github.technus.tectech.mechanics.structure.StructureDefinition;
+import com.github.technus.tectech.thing.metaTileEntity.multi.base.GT_MetaTileEntity_MultiblockBase_EM;
 import com.github.technus.tectech.util.Vec3Impl;
 import com.gtnewhorizons.gtppnt.main.tileentites.multi.definition.GT_MetaTileEntity_TM_Factory;
+import com.gtnewhorizons.gtppnt.main.tileentites.multi.definition.structure.IConstructableStructureCells;
 import com.gtnewhorizons.gtppnt.main.tileentites.multi.definition.structure.IConstructableStructureSliceable;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.util.GT_Recipe;
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 
 import static com.github.technus.tectech.mechanics.structure.StructureUtility.*;
 import static com.github.technus.tectech.thing.casing.TT_Container_Casings.sHintCasingsTT;
 import static com.gtnewhorizons.gtppnt.main.compat.bartworks.MaterialsClass.MaragingSteel250;
 
-public class GT_MetaTileEntity_TM_Large_Centrifuge extends GT_MetaTileEntity_TM_Factory implements IConstructableStructureSliceable {
-    //region Constructors
+public class GT_MetaTileEntity_TM_Large_Centrifuge extends GT_MetaTileEntity_TM_Factory implements IConstructableStructureCells {
+    int paralells = 0;
+
     public GT_MetaTileEntity_TM_Large_Centrifuge(int aID) {
         super(aID, "multimachine.tm.large_centrifuge", "Large Centrifuge");//TODO Set cooler name + .lang
     }
@@ -35,92 +39,136 @@ public class GT_MetaTileEntity_TM_Large_Centrifuge extends GT_MetaTileEntity_TM_
     public IMetaTileEntity newMetaEntity(IGregTechTileEntity iGregTechTileEntity) {
         return new GT_MetaTileEntity_TM_Large_Centrifuge(mName);
     }
-    //endregion
 
-    //region Structure
+    @Override
+    public IStructureDefinition<? extends GT_MetaTileEntity_MultiblockBase_EM> getMachineStructure() {
+        Block reinforcedGlass = Block.getBlockFromName("IC2:blockAlloyGlass");
+        return StructureDefinition.<GT_MetaTileEntity_TM_Factory>builder()
+                .addShape(TM_STRUCTURE_A,new String[][]{
+                        {" cc "," A~ "," BB "},
+                        {"cmmc","A--A","BBBB"},
+                        {"cmmc","A--A","BBBB"},
+                        {" cc "," AA "," BB "}
+                })
+                .addShape(TM_STRUCTURE_B,new String[][]{
+                        {" cBc "," A~A "," cBc "},
+                        {"cmmmc","A---A","cBBBc"},
+                        {"BmmmB","A---A","BBBBB"},
+                        {"cmmmc","A---A","cBBBc"},
+                        {" cBc "," AAA "," cBc "}
+                })
+                .addShape(TM_STRUCTURE_C,new String[][]{
+                        {"  ccc  ","  A~A  ","  ccc  "},
+                        {" cmmmc "," A---A "," cBBBc "},
+                        {"cmmBmmc","A-----A","cBBBBBc"},
+                        {"cmBBBmc","A-----A","cBBBBBc"},
+                        {"cmmBmmc","A-----A","cBBBBBc"},
+                        {" cmmmc "," A---A "," cBBBc "},
+                        {"  ccc  ","  AAA  ","  ccc  "}
+                })
+                .addShape(TM_STRUCTURE_D,new String[][]{
+                        {"  ccccc  ","  AA~AA  ","  ccccc  "},
+                        {" ccmmmcc "," A-----A "," ccmmmcc "},
+                        {"ccmBBBmcc","A-------A","ccmBBBmcc"},
+                        {"cmBBBBBmc","A-------A","cmBBBBBmc"},
+                        {"cmBBBBBmc","A-------A","cmBBBBBmc"},
+                        {"cmBBBBBmc","A-------A","cmBBBBBmc"},
+                        {"ccmBBBmcc","A-------A","ccmBBBmcc"},
+                        {" ccmmmcc "," A-----A "," ccmmmcc "},
+                        {"  ccccc  ","  AAAAA  ","  ccccc  "}
+                })
+                .addElement('A', ofBlock(getCasingBlock(), getCasingMeta()))
+                .addElement('G', ofBlockAnyMeta(reinforcedGlass))
+                .addElement('B',ofChain(
+                        ofHatchAdder(GT_MetaTileEntity_TM_Factory::addClassicToMachineList,
+                                getTextureIndex(),1),
+                        ofBlock(getCasingBlock(), getCasingMeta())))
+                .addElement('m',ofHatchAdder(GT_MetaTileEntity_TM_Factory::addMotorCasingToMachineList,
+                        getTextureIndex(),2))
+                .addElement('c',ofHatchAdder(GT_MetaTileEntity_TM_Factory::addCircuitCasingToMachineList,
+                        getTextureIndex(),3))
+                .build();
+    }
+
+    @Override
+    public Vec3Impl getStartOffsetA() {
+        return new Vec3Impl(2,1,0);
+    }
+
+    @Override
+    public Vec3Impl getStartOffsetB() {
+        return new Vec3Impl(2,1,0);
+    }
+
+    @Override
+    public Vec3Impl getStartOffsetC() {
+        return new Vec3Impl(3,1,0);
+    }
+
+    @Override
+    public Vec3Impl getStartOffsetD() {
+        return new Vec3Impl(4,1,0);
+    }
+
+    @Override
+    public int getParalellsA() {
+        return 32;
+    }
+
+    @Override
+    public int getParalellsB() {
+        return 64;
+    }
+
+    @Override
+    public int getParalellsC() {
+        return 128;
+    }
+
+    @Override
+    public int getParalellsD() {
+        return 256;
+    }
+
+    @Override
+    public void setParalellsABCD(int paralells) {
+        this.paralells = paralells;
+    }
+
+    @Override
+    public int getParalellsABCD() {
+        return paralells;
+    }
+
     @Override
     public short getCasingMeta() {
         return MaragingSteel250.getmID();
     }
 
-    @Override
-    public IStructureDefinition<GT_MetaTileEntity_TM_Factory> getMachineStructure() {
-        return StructureDefinition.<GT_MetaTileEntity_TM_Factory>builder()
-                .addShape(TM_STRUCTURE_START, new String[][]{
-                        {"BBBB", "BB~B", "BBBB"},
-                }) // 2 1 0
-                .addShape(TM_STRUCTURE_MIDDLE, new String[][]{
-                        {"AmmA", "c  A", "AAAA",},
-                        {"AmmA", "c  A", "AAAA",},
-                        {"AAAA", "AAAA", "AAAA",},
-                }) // 2 1 -1
-                .addElement('A', ofBlock(getCasingBlock(), getCasingMeta()))
-                .addElement('B', ofChain(
-                        ofHatchAdder(GT_MetaTileEntity_TM_Factory::addClassicToMachineList,
-                                getTextureIndex(), sHintCasingsTT, 0),
-                        ofBlock(getCasingBlock(), getCasingMeta())
-                ))
-                .addElement('c', ofHatchAdder(GT_MetaTileEntity_TM_Factory::addCircuitCasingToMachineList,
-                        getTextureIndex(), sHintCasingsTT, 1)
-                )
-                .addElement('m', ofHatchAdder(GT_MetaTileEntity_TM_Factory::addMotorCasingToMachineList,
-                        getTextureIndex(), sHintCasingsTT, 2)
-                )
-                .build();
-    }
-
-    @Override
-    public Vec3Impl getStartStructureOffset() {
-        return new Vec3Impl(2, 1, 0);
-    }
-
-    @Override
-    public Vec3Impl getSliceStructureOffset() {
-        return new Vec3Impl(2, 1, -1);
-    }
-
-    @Override
-    public Vec3Impl getPerSliceOffset() {
-        return new Vec3Impl(0, 0, -3);
-    }
-
-    @Override
-    public int getMaxSlices() {
-        return 4;
-    }
-
-    @Override
-    public int getParalellsPerSlice() {
-        return 32;
-    }
-
-    @Override
     @SideOnly(Side.CLIENT)
+    @Override
     public String[] getStructureDescription(ItemStack itemStack) {
-        return new String[]{"CHANGE-ME"}; // TODO fix the description
+        return new String[]{
+                "1 - Classic Hatch",
+                "2 - Motor Hatch",
+                "3 - Circuit Hatch"
+        };
     }
-    //endregion
 
-    //region Textures
-    @Override
     @SideOnly(Side.CLIENT)
+    @Override
     public String getMachineTextureName() {
         return "TM_LARGE_CENTRIFUGE";
     }
-    //endregion
 
-    //region Sounds
-    @Override
     @SideOnly(Side.CLIENT)
+    @Override
     public String getMachineSoundName() {
-        return "fx_lo_freq"; // assets/tectech/sounds/fx_lo_freq.ogg
+        return "fx_lo_freq";
     }
-    //endregion
 
-    //region On Tick
     @Override
     public GT_Recipe.GT_Recipe_Map getRecipeMap() {
         return GT_Recipe.GT_Recipe_Map.sCentrifugeRecipes;
     }
-    //endregion
 }
